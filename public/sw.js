c49f73844caac2a2e08ef4bf2ac34b75c45c1abf
shoebalam440 <shoebalam440@gmail.com>
2025-08-27 22:22:21 +0530

    Fix PWA issues: add mobile-web-app-capable meta tag, fix service worker cache error, simplify manifest icons

--- public/sw.js
+++ public/sw.js
@@ -55,10 +55,13 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(
       fetch(request)
         .then((response) => {
-          const responseClone = response.clone();
-          caches.open(DYNAMIC_CACHE).then((cache) => {
-            cache.put(request, responseClone);
-          });
+          // Only cache GET requests
+          if (request.method === 'GET') {
+            const responseClone = response.clone();
+            caches.open(DYNAMIC_CACHE).then((cache) => {
+              cache.put(request, responseClone);
+            });
+          }
           return response;
         })
         .catch(() => {
